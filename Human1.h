5e15ad503a7de157b3bd0c49c54002d8f2864335
lilinepp <129063961+lilinepp@users.noreply.github.com>
2023-05-04 17:13:29 +0900

    Update Human1.h

--- Human1.h
+++ Human1.h
@@ -38,20 +38,21 @@ public:
 	Human1();
 
 	// конструктор с заданием данных
-	Human1(string _name,string _surname, string _patron, int _age, int _weight);
+	Human1(const string &_name,const string &_surname,const string& _patron, int _age, int _weight);
 
-	string get_surname() const;
-	void set_surname(string surname1);
+	string get_surname() const;//const-изменение формального параметра внутри функции не требуется,также нужен для вызова  объекта-константы
+	void set_surname(const string &surname1);
 	string get_patron() const;
-	void set_patron(string patron1);
+	void set_patron(const string &patron1);
 	int get_age() const;///возвращает возраст
 	int get_weight() const;//возвращает вес
 	void set_age(int age1); //заполняет возраст
 	void set_weight(int weight1);//заполняет вес
 	string get_name() const;//возращает ФИО
 	void set_name(string name1);//заполняет ФИО
-	string tostring() const;
-	void set_all(string _name, string _surname, string _patron, int _age, int _weight); 
+	string tostring() const;//перевод полей в строку
+	void set_all(const string &_name,const string &_surname,const string &_patron, int _age, int _weight); //общий сеттер
 
 };
+
 
