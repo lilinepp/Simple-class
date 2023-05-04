e8379effc9a6f26eca4fc6b55d4cce0da3d76cdd
lilinepp <129063961+lilinepp@users.noreply.github.com>
2023-05-04 17:17:25 +0900

    Update test.cpp

--- test.cpp
+++ test.cpp
@@ -11,7 +11,7 @@ TEST(TestHuman, TesAge) {
 	{
 		Human1 h;
 		h.set_age(66);
-		EXPECT_EQ(h.get_age(), 65);
+		EXPECT_EQ(h.get_age(), 66);
 	}
  
 }
@@ -27,7 +27,7 @@ TEST(TestHuman, TesWeight) {
 	{
 		Human1 h;
 		h.set_weight(67);
-		EXPECT_EQ(h.get_weight(), 65);
+		EXPECT_EQ(h.get_weight(), 67);
 	}
 
 }
@@ -45,7 +45,7 @@ TEST(TestHuman, TesName) {
 	{
 		Human1 h;
 		h.set_name("Илья");
-		EXPECT_EQ(h.get_name(), "Пётр");
+		EXPECT_EQ(h.get_name(), "Илья");
 	}
 
 }
@@ -77,8 +77,25 @@ TEST(TestHuman, TesPatron) {
 	{
 		Human1 h;
 		h.set_name("Петрович");
-		EXPECT_EQ(h.get_name(), "Ильич");
+		EXPECT_EQ(h.get_name(), "Петрович");
 	}
 
 }
+
+
+TEST(TestHuman, TesToString) {
+
+	{
+		Human1 h;
+		string line="Петров Пётр Петрович возраст 55 вес 66\n";
+		h.set_surname("Петров");
+		h.set_name("Пётр");
+		h.set_patron("Петрович");
+		h.set_age(55);
+	    h.set_weight(66);;
+		EXPECT_EQ(h.tostring(),line);
+	}
+
+}
+
 
